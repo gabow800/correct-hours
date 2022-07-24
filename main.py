8ea72eafb44c70a5617cd2808212192aefedfb84
Jose Orozco <jose.orozco@service.vic.gov.au>
2022-07-24 16:34:41 +1000

    fix(App): correct hours logic

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ original_sheet = workbook.active
 
 new_sheet = workbook.copy_worksheet(from_worksheet=original_sheet)
 
-
 def add_up_hours(current_week_ending, row_start):
     rows_added = 0
     total_hours = 0
@@ -23,21 +22,19 @@ def add_up_hours(current_week_ending, row_start):
 def correct_hours(row_start, row_offset, overtime):
     print("Hours need to be corrected")
     time_left = overtime
-    for i in range(13, 7, -1):
-        for j in range((row_start + row_offset) - 1, row_start - 1, -1):
-            value = new_sheet.cell(j, i).value
+    for col_idx in range(13, 7, -1):
+        for row_idx in range((row_start + row_offset) - 1, row_start - 1, -1):
+            value = new_sheet.cell(row_idx, col_idx).value
             if value <= 0:
                 continue
             corrected_value = value - time_left
             if corrected_value < 0:
                 time_left = corrected_value * -1
                 corrected_value = 0
-                new_sheet.cell(i, j, corrected_value)
+                new_sheet.cell(row_idx, col_idx, corrected_value)
             else:
-                new_sheet.cell(i, j, corrected_value)
+                new_sheet.cell(row_idx, col_idx, corrected_value)
                 return
-
-
 
 
 row_count = 0
@@ -45,13 +42,12 @@ for idx, row in enumerate(new_sheet.iter_rows(min_row=6, min_col=1, max_col=17, 
     if idx < row_count:
         continue
     week_ending = row[0]
-    rows_added, total_hours = add_up_hours(week_ending, idx + 6)
-    total_hours_cell = f"O{idx + 6}"
-    new_sheet[total_hours_cell] = total_hours
+    row_idx = idx + 6
+    rows_added, total_hours = add_up_hours(week_ending, row_idx)
+    new_sheet.cell(row_idx, 15, total_hours)
     if total_hours > 38:
         overtime = total_hours - 38
-        correct_hours(idx + 6, rows_added, overtime)
+        correct_hours(row_idx, rows_added, overtime)
     row_count += rows_added
-
 
 workbook.save(filename="timesheets/callum2.xlsx")
