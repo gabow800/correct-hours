31b9d542753fb581f8b385d74d3193e9e564c135
Jose Orozco <jose.orozco@service.vic.gov.au>
2022-07-24 17:48:41 +1000

    fix(App): days worked logic

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from openpyxl import load_workbook, Workbook
+from openpyxl import load_workbook
+from openpyxl.styles import Font
 
 workbook = load_workbook(filename="timesheets/callum.xlsx")
 original_sheet = workbook.active
-
 new_sheet = workbook.copy_worksheet(from_worksheet=original_sheet)
+
+# highlight style
+# highlight = NamedStyle(name="highlight")
+# highlight.font = Font(bold=True, size=20)
+# bd = Side(style='thick', color="000000")
+# highlight.border = Border(left=bd, top=bd, right=bd, bottom=bd)
+# workbook.add_named_style(highlight)
+
+# new column headings
+new_sheet.cell(5, 15, "Old total").font = Font(bold=True)
+new_sheet.cell(5, 16, "New total").font = Font(bold=True)
+new_sheet.cell(5, 17, "Days worked").font = Font(bold=True)
 
 def add_up_hours(current_week_ending, row_start):
     rows_added = 0
@@ -31,23 +43,34 @@ def correct_hours(row_start, row_offset, overtime):
             if corrected_value < 0:
                 time_left = corrected_value * -1
                 corrected_value = 0
-                new_sheet.cell(row_idx, col_idx, corrected_value)
+                new_sheet.cell(row_idx, col_idx, corrected_value).font = Font(bold=True, color="FF0000")
             else:
-                new_sheet.cell(row_idx, col_idx, corrected_value)
+                new_sheet.cell(row_idx, col_idx, corrected_value).font = Font(bold=True, color="FF0000")
                 return
 
 
 row_count = 0
-for idx, row in enumerate(new_sheet.iter_rows(min_row=6, min_col=1, max_col=17, values_only=True)):
+for idx, row in enumerate(new_sheet.iter_rows(min_row=6, min_col=1, max_col=14, values_only=True)):
     if idx < row_count:
         continue
     week_ending = row[0]
-    row_idx = idx + 6
-    rows_added, total_hours = add_up_hours(week_ending, row_idx)
-    new_sheet.cell(row_idx, 15, total_hours)
+    start_row_idx = idx + 6
+    rows_added, total_hours = add_up_hours(week_ending, start_row_idx)
+    end_row_idx = (start_row_idx + (rows_added - 1))
+    # old total of hours
+    new_sheet.cell(end_row_idx, 15, total_hours)
+    did_overtime = False
     if total_hours > 38:
         overtime = total_hours - 38
-        correct_hours(row_idx, rows_added, overtime)
+        correct_hours(start_row_idx, rows_added, overtime)
+        did_overtime = True
     row_count += rows_added
+    # new total of hours
+    new_total_of_hours = new_sheet.cell(end_row_idx, 16, f"=SUM(G{start_row_idx}:M{end_row_idx})")
+    if did_overtime:
+        new_total_of_hours.font = Font(bold=True, color="FF0000")
+    # number of days worked
+    new_sheet.cell(end_row_idx, 17, f"=P{end_row_idx}/7.6")
+
 
 workbook.save(filename="timesheets/callum2.xlsx")
